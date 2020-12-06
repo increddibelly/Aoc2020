068fa45c7ab61efea1ddb950a0804cd666f8d5c2
Joost Boomkamp <joostboomkamp@yoast.com>
2020-12-06 08:24:02 +0100

    fix styling

--- AOC2020/Day05/BoardingPass.cs
+++ AOC2020/Day05/BoardingPass.cs
@@ -1,44 +1,9 @@
-﻿using System;
-using System.Linq;
-
-namespace Day05
+﻿namespace Day05
 {
     public class BoardingPass
     {
-        // BFFFBBFRRR: row 70, column 7, seat ID 567.
-        // FFFBBBFRRR: row 14, column 7, seat ID 119.
-        // BBFFBBFRLL: row 102, column 4, seat ID 820.
         public int Row { get; set; }
         public int Column { get; set; }
         public int SeatID => Row * 8 + Column;
-    }
-
-    public class BoardingPassParser
-    {
-        public static BoardingPass Parse(string input)
-        {
-            var rowData = input.Substring(0, 7);
-            var columnData = input.Substring(7, 3);
-
-            var row = BinaryAdapter(rowData, "F", "B");
-            var column = BinaryAdapter(columnData, "L", "R");
-
-            return new BoardingPass
-            {
-                Row = row,
-                Column = column
-            };
-        }
-
-        public static BoardingPass[] ParseMany(string input)
-        {
-            return input.Split(Environment.NewLine).Select(Parse).ToArray();
-        }
-
-        private static int BinaryAdapter(string input, string zero, string one) {
-            var binaryData = input.Replace(zero, "0").Replace(one, "1");
-            var data = Convert.ToInt32(binaryData, 2);
-            return data;
-        }
     }
 }
---
+++ AOC2020/Day05/BoardingPassParser.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace Day05
+{
+    public class BoardingPassParser
+    {
+        public static BoardingPass Parse(string input)
+        {
+            var rowData = input.Substring(0, 7);
+            var columnData = input.Substring(7, 3);
+
+            var row = BinaryAdapter(rowData, "F", "B");
+            var column = BinaryAdapter(columnData, "L", "R");
+
+            return new BoardingPass
+            {
+                Row = row,
+                Column = column
+            };
+        }
+
+        public static BoardingPass[] ParseMany(string input)
+        {
+            return input.Split(Environment.NewLine).Select(Parse).ToArray();
+        }
+
+        private static int BinaryAdapter(string input, string zero, string one) {
+            var binaryData = input.Replace(zero, "0").Replace(one, "1");
+            var data = Convert.ToInt32(binaryData, 2);
+            return data;
+        }
+    }
+}
